4d0c2a906f1511a1857ecd128118d0185400c05c
kaif <103161771+SMKAIF12@users.noreply.github.com>
2026-05-07 16:47:31 +0530

    Updated connection

--- api/database-connection/connection.js
+++ api/database-connection/connection.js
@@ -1,40 +1,12 @@
-require('dotenv').config();
+require('dotenv').config()
 const mongoose = require('mongoose');
-
-let cached = global.mongoose;
-
-if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null };
-}
-
 const connectDB = async () => {
-    const uri = process.env.URL;
-    if (!uri) {
-        throw new Error("Please define the URL environment variable inside .env or Vercel Settings");
-    }
-    if (cached.conn) {
-        return cached.conn;
-    }
-    if (!cached.promise) {
-        const opts = {
-            bufferCommands: false,
-            connectTimeoutMS: 10000,
-            socketTimeoutMS: 45000,
-            family: 4
-        };
-        cached.promise = mongoose.connect(uri, opts).then((mongoose) => {
-            console.log('MongoDB connection successful.');
-            return mongoose;
-        });
-    }
     try {
-        cached.conn = await cached.promise;
-    } catch (e) {
-        cached.promise = null;
-        console.error('MongoDB connection Failed: ', e);
-        throw e;
+        await mongoose.connect(process.env.URL);
+        console.log('MongoDB connection successful.');
+    } catch (error) {
+        console.error('MongoDB connection Failed: ', error);
+        process.exit(1);
     }
-    return cached.conn;
-};
-
+}
 module.exports = connectDB;
