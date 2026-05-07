5e40aa704ccdf806cb3bc45cb36758e7716870b4
kaif <103161771+SMKAIF12@users.noreply.github.com>
2026-05-07 17:30:49 +0530

    updated server.js

--- api/server.js
+++ api/server.js
@@ -9,7 +9,7 @@ connectDB();
 const app = express();
 app.use(express.json());
 // Allow all origins
-const allowedOrigins = ["https://expense-tracker-bice-rho.vercel.app"];
+const allowedOrigins = ["https://expense-tracker-gl5h.vercel.app/","http://localhost:5173/"];
 
 app.use(cors({
   origin: allowedOrigins,
